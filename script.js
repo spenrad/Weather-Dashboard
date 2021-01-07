1af7ad1da854ab5735c5eb474ffff7b1feb12347
Spencer Christy <spenrad@gmail.com>
2021-01-07 01:49:11 -0800

    added luxon and converted unix time from weather api to MM/DD/YYYY format, overwrote h2 text with city and date info from both apis

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+var DateTime = luxon.DateTime;
+var time = DateTime.local();
+
 function getCurrent() {
   var cityName = "San+Francisco";
   //   var cityName = "";
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=40e2dec5462a7caaff123f43a6aa58d6";
-  
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=40e2dec5462a7caaff123f43a6aa58d6";
+
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function(data){
-    console.log(data)
+  }).then(function (data) {
+    console.log(data);
+    var time = DateTime.fromSeconds(data.dt).toLocaleString(
+      DateTime.DATE_SHORT
+    );
+    $("h2").text(data.name + " " + time);
   });
 }
 
