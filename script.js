07596fccde7e95cf3257361853af57558f99600c
Spencer Christy <spenrad@gmail.com>
2021-01-07 01:44:50 -0800

    successfully pulled api data into console log for current weather

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function getCurrent() {
   var cityName = "San+Francisco";
-  var queryURL = "api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=40e2dec5462a7caaff123f43a6aa58d6";
+  //   var cityName = "";
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=40e2dec5462a7caaff123f43a6aa58d6";
   
   $.ajax({
     url: queryURL,
     method: "GET",
+  }).then(function(data){
+    console.log(data)
   });
 }
 
